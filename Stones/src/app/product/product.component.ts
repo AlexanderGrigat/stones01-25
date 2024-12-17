b8c9b042134276e0e3eeaea6c87cad54e894432d
Alexander Grigat <alexander.grigat@sn-invent.de>
2024-12-17 16:08:00 +0100

    Aufgabe 3

--- Stones/src/app/product/product.component.ts
+++ Stones/src/app/product/product.component.ts
@@ -10,13 +10,23 @@ import { Product } from './product';
 export class ProductComponent {
   @Input({required: true}) product!: Product;
   @Output() priceChange = new EventEmitter<number>();
+
+  showPrice = true;
+  styleConfig: any = {
+    'border-style': 'dashed',
+  }
+
   raisePrice(){
     this.product.price += 5;
     this.priceChange.emit(this.product.price);
   }
 
-  changePrice(price: number): void{
-    this.product.price = price;
-    this.priceChange.emit(this.product.price);
+  togglePrice() {
+    this.showPrice = !this.showPrice;
   }
+
+  // changePrice(price: number): void{
+  //   this.product.price = price;
+  //   this.priceChange.emit(this.product.price);
+  // }
 }
